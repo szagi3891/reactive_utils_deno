4c1a178b42fad7688f6bcfe2388227fa1be07185
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2025-02-21 10:05:04 +0100

    version 0.0.9

--- src/git.ts
+++ src/git.ts
@@ -1,6 +1,6 @@
 import { execAndGet } from "./exec.ts";
 
-export const getBranchCommitCount = async (cwd: string, branch: string): Promise<number> => {    
+export const branchCommitCount = async (cwd: string, branch: string): Promise<number> => {    
     const result = await execAndGet(cwd, `git rev-list --count ${branch}`);
 
     const count = Number(result);
@@ -10,16 +10,21 @@ export const getBranchCommitCount = async (cwd: string, branch: string): Promise
     return count;
 };
 
-export const getGitCommits = async (cwd: string, branch: string): Promise<Array<string>> => {
+export const branchCommits = async (cwd: string, branch: string): Promise<Array<string>> => {
     const result = await execAndGet(cwd, `git log ${branch} --pretty=format:%H`);
 
     const lines = result.split('\n').filter((line) => (line.trim() === '' ? false : true));
 
-    const count = await getBranchCommitCount(cwd, branch);
+    const count = await branchCommitCount(cwd, branch);
     if (lines.length !== count) {
         throw Error('problem with reading the list of commits');
     }
 
     return lines;
+};
+
+export const branchName = async (cwd: string): Promise<string> => {
+    const branchName = await execAndGet(cwd, 'git rev-parse --abbrev-ref HEAD');
+    return branchName.trim();;
 };
 
